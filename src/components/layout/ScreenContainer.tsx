1819177ec728fea778a466ce9151baf38e954139
a-neey <2371429@hansung.ac.kr>
2026-08-02 22:42:33 +0900

    💄 Style: 메인 레이아웃 구현

---
+++ src/components/layout/ScreenContainer.tsx
@@ -0,0 +1,9 @@
+const ScreenContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen bg-[#FCFCFC]">
+      <div className="min-h-screen w-full">{children}</div>
+    </div>
+  );
+};
+
+export default ScreenContainer;
