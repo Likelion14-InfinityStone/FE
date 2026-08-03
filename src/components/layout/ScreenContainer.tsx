4c6b26a51477fd92c5270f816ba38e7be75aac27
a-neey <2371429@hansung.ac.kr>
2026-08-03 23:20:07 +0900

    🐛 Fix: 피드백 반영

--- src/components/layout/ScreenContainer.tsx
+++ src/components/layout/ScreenContainer.tsx
@@ -1,7 +1,9 @@
+import type React from 'react';
+
 const ScreenContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative mx-6.5 flex w-full max-w-100.5 flex-col items-start justify-start bg-[#FCFCFC]">
-      <div className="w-full flex-1 p-0">{children}</div>
+    <div className="relative flex w-full flex-col items-start justify-start px-6.5">
+      <div className="w-full flex-1">{children}</div>
     </div>
   );
 };
