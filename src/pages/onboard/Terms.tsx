e5b4c9003ff6ee6d67701f96a4c61dc1ddd1ba09
a-neey <2371429@hansung.ac.kr>
2026-08-04 00:54:27 +0900

    🚚 Route: 약관 동의 페이지 라우팅 추가

---
+++ src/pages/onboard/Terms.tsx
@@ -0,0 +1,5 @@
+const Terms = () => {
+  return <p>약관 동의 화면</p>;
+};
+
+export default Terms;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '@/components/layout/MainLayout';
 import Splash from '@/components/splash/Splash';
 import Onboard from '@/pages/onboard/Onboard';
+import Terms from '@/pages/onboard/Terms';
 import Login from '@/pages/login/Login';
 import Home from '@/pages/home/Home';
 import Register from '@/pages/register/Register';
@@ -12,11 +13,13 @@ import Emergency from '@/pages/emergency/Emergency';
 import NotFound from '@/pages/notFound/NotFound';
 
 const router = createBrowserRouter([
+  { path: '/', element: <Splash /> },
+
   {
     element: <MainLayout />,
     children: [
-      { path: '/', element: <Splash /> },
       { path: '/onboard', element: <Onboard /> },
+      { path: '/terms', element: <Terms /> },
       { path: '/login', element: <Login /> },
       // { path: '/oauth/callback', element: <OAuthCallback /> },
 
