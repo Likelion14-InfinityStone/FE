24637f9258eb4c87425094beb5313f131dac51b2
a-neey <2371429@hansung.ac.kr>
2026-08-04 10:29:26 +0900

    🚚 Route: 온보딩 이후 홈으로 연결

--- src/pages/onboard/Terms.tsx
+++ src/pages/onboard/Terms.tsx
@@ -49,7 +49,7 @@ const Terms = () => {
       onToggleAgreement={toggleTermAgreement}
       onSelectTerm={(termId) => setSearchParams({ detail: termId })}
       onBack={() => navigate('/onboard')}
-      onConfirm={() => navigate('/login')}
+      onConfirm={() => navigate('/home')}
     />
   );
 };
