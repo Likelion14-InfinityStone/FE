de3273b0f5a8bf9875e7b56ee235a3598f9e569c
a-neey <2371429@hansung.ac.kr>
2026-08-05 15:27:09 +0900

    ✨ Feat: SOS 버튼 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,27 @@
 import Header from '@/components/layout/Header';
 import MedicineCard from './components/MedicineCard';
+import SosPiruIcon from '@/assets/images/home/sosPiruIcon.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full h-full">
       <Header title="복약 카드" />
       <MedicineCard name="피루피루" status="unregistered" />
+      <div className="pointer-events-none fixed inset-x-0 bottom-29 z-10 mx-auto w-full max-w-100.5 px-4">
+        <button
+          type="button"
+          onClick={() => navigate('/emergency')}
+          className="pointer-events-auto ml-auto flex h-16 w-16 p-2.5 flex-col items-center justify-center rounded-full bg-[#EF5050]"
+        >
+          <p className="font-Pretendard text-[1.375rem] leading-none font-semibold tracking-[-0.5px] text-white">
+            SOS
+          </p>
+          <img src={SosPiruIcon} alt="" className="block" />
+        </button>
+      </div>
     </div>
   );
 };
