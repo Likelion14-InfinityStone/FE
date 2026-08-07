8965f6f46468e48d64adb91de78927b218cc0319
a-neey <2371429@hansung.ac.kr>
2026-08-07 15:43:49 +0900

    ♻️ Refactor: 하단 버튼 분리

--- src/components/button/BottomButton.tsx
+++ src/components/button/BottomButton.tsx
@@ -1,13 +1,11 @@
 type BottomButtonProps = {
   text: string;
-  backgroundColor?: string;
   onClick?: () => void;
   disabled?: boolean;
 };
 
 const BottomButton = ({
   text,
-  backgroundColor = '#23408F',
   onClick,
   disabled = false,
 }: BottomButtonProps) => {
@@ -15,7 +13,6 @@ const BottomButton = ({
     <button
       type="button"
       className="w-full rounded-[20px] bg-[#23408F] py-5 disabled:cursor-not-allowed disabled:bg-[#23408F]/40"
-      style={disabled ? undefined : { backgroundColor }}
       onClick={onClick}
       disabled={disabled}
     >
