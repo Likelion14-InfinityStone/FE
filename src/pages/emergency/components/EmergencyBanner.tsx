cbc738a4f7365d0010f06c7eed8720936a52c785
a-neey <2371429@hansung.ac.kr>
2026-08-07 14:09:08 +0900

    ✨ Feat: SOS 배너 컴포넌트 구현

---
+++ src/pages/emergency/components/EmergencyBanner.tsx
@@ -0,0 +1,17 @@
+interface EmergencyBannerProps {
+  icon: string;
+  title: string;
+}
+
+const EmergencyBanner = ({ icon, title }: EmergencyBannerProps) => {
+  return (
+    <div className="flex min-h-15 items-center justify-center gap-2 rounded-[18px] bg-[#EF5050] py-5 text-center">
+      <img src={icon} alt="" />
+      <p className="font-Pretendard text-[1.125rem] leading-6.3 font-semibold text-[#FAFAF6]">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default EmergencyBanner;
