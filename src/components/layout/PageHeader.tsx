f61cfa000b39bb28a9d0fbbaf84f72457444b3c7
a-neey <2371429@hansung.ac.kr>
2026-08-07 13:38:41 +0900

    ✨ Feat: PageHeader 공통 컴포넌트 추가

---
+++ src/components/layout/PageHeader.tsx
@@ -0,0 +1,27 @@
+import BackArrowIcon from '@/assets/images/onboard/backArrowIcon.svg';
+import { useNavigate } from 'react-router-dom';
+
+type PageHeaderProps = {
+  title: string;
+};
+
+const PageHeader = ({ title }: PageHeaderProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex relative pt-5.5 pb-5 items-center justify-center">
+      <button
+        type="button"
+        onClick={() => navigate(-1)}
+        className="absolute left-0 items-center"
+      >
+        <img src={BackArrowIcon} alt="뒤로 가기" />
+      </button>
+      <p className="font-Pretendard text-[1.375rem] leading-7.5 font-semibold text-[#191919]">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default PageHeader;
