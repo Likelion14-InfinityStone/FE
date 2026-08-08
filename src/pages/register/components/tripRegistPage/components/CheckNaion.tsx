7af23b0aa8982e2cc4294934955bb8acf1de8862
atelierhs <changhs1227@gmail.com>
2026-08-09 00:29:59 +0900

    🐛 Fix: 피드백 버튼 추가

--- src/pages/register/components/tripRegistPage/components/CheckNaion.tsx
+++ src/pages/register/components/tripRegistPage/components/CheckNaion.tsx
@@ -12,7 +12,7 @@ const CheckNaion = ({ label, active = false, onClick }: CheckNaionProps) => {
       className={`flex h-[42px] items-center justify-center rounded-[24px] px-4 py-2.5 font-Pretendard text-[1rem] leading-[1.4] tracking-[0.384px] ${
         active
           ? 'bg-[#23408F] text-[#FAFAF6]'
-          : 'border-2 border-[#E2E2E2] text-[#848B9C]'
+          : 'border-2 border-[#E2E2E2] text-[#848B9C] hover:border-[#23408F]'
       }`}
     >
       {label}
