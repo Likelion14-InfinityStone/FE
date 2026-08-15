bb6a8c55191ef2691aa97ec48b14fbcf912ccb58
a-neey <2371429@hansung.ac.kr>
2026-08-15 23:20:07 +0900

    ✨ Feat: 인스턴스 설정

---
+++ src/apis/instance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// 공통 인스턴스
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
